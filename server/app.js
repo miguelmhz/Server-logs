d69c108fc783c3e9ed9d86783e4db9f5900cf816
¨Miguel <miguelmhz1905@gmail.com>
2024-02-19 17:48:03 -0600

    server logs

---
+++ server/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const { exec } = require('child_process');
+const fs = require('fs');
+
+// Función para ejecutar un comando del sistema y retornar una promesa con su salida
+const executeCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        resolve(stderr);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+};
+
+  
+
+// Ruta para obtener el informe del servidor
+app.get('/api/report',async (req, res) => {
+  try {
+    // Obtener la lista de procesos en ejecución
+    const psOutput = await executeCommand('ps aux');
+    const pm2Output = await executeCommand('pm2 list');
+
+    // Obtener la lista de puertos en uso
+    const netstatOutput = await executeCommand('netstat -tuln');
+
+    // Obtener el estado de la memoria
+    const memoryOutput = await executeCommand('free -m');
+
+    // Obtener el estado de la CPU
+    const cpuOutput = await executeCommand('top -n 1 -b');
+    // Guardar el contenido HTML en un archivo
+    const reportData = {
+        processes: psOutput,
+        pm2: pm2Output,
+        ports: netstatOutput,
+        memory:memoryOutput,
+        cpu: cpuOutput
+    };
+
+    res.json(reportData);
+  } catch (error) {
+    console.error('Error generating server report:', error);
+  }
+});
+app.get('/hi',(req, res)=> {
+  res.json({
+      ok:true,
+  })
+})
+// Iniciar el servidor en el puerto 3000
+const PORT = process.env.PORT || 3010;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
